d81a9a09a2e048599a825a19a8a6e723254a2563
ilyauzbek <Stas.yun.82@gmail.com>
2024-10-28 17:13:00 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import './App.scss'
+
+
+import Hero from './components/Hero/Hero'
+import Catalog from './components/Catalog/Catalog'
+
+const App = () => {
+  return (
+    <>
+   
+   <Header/>
+   <Hero/>
+   <Catalog/>
+    
+    </>
+  )
+}
+
+export default App
